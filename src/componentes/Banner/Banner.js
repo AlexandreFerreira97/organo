52975ca2d1174752e60fcbd4873d7525dc1dd2a0
Alexandre <alexandrefaria3012@gmail.com>
2024-08-05 18:50:01 -0300

    Update Banner.js

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,8 @@
+import banner from "../../img/banner.png"
+
 function Banner() {
     return(
-        <img src="../imagens/banner.png" alt="Banner principal da página do Organo"/>
+        <img src={banner} alt="Banner principal da página do Organo"/>
     )
 }
 
