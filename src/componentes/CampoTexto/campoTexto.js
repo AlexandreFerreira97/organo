0b36bf63c3fb38322ed684ac9d00318a65900293
Alexandre <alexandrefaria3012@gmail.com>
2024-08-05 22:31:32 -0300

    Update campoTexto.js

--- src/componentes/CampoTexto/campoTexto.js
+++ src/componentes/CampoTexto/campoTexto.js
@@ -1,5 +1,13 @@
-const CampoTexto = () => {
-    
+import './campoTexto.css'
+
+const CampoTexto = (props) => {
+    //jsx
+    return(
+        <div className="campo-texto">
+            <label className="label_campo-texto">{props.label}</label>
+            <input className='ipt' placeholder={props.placeholder}/>
+        </div>
+    )
 }
 
 export default CampoTexto
