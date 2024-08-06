d6a0e63938b7aa1c6abf1dddbece6dfa8c9483ab
Alexandre <alexandrefaria3012@gmail.com>
2024-08-05 22:31:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './componentes/Banner/banner';
+import CampoTexto from './componentes/CampoTexto/campoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite o cargo"/>
+      <CampoTexto label="Imagem"/>
     </div>
   );
 }
