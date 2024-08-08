8d0f73eea0dba45e6f8d4d5c0c11672c6c2b98ee
Alexandre <alexandrefaria3012@gmail.com>
2024-08-08 16:37:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,59 @@ import Time from './componentes/Time/time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Front-end',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'UX & Design',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '',
+      corSecundaria: ''
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdd = (colaborador) => {
 
-    setColaboradores([...colaboradores,colaborador])
+    setColaboradores([...colaboradores, colaborador])
 
   }
 
   return (
     <div className="App">
-      <Banner/>
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdd(colaborador)}/>
-      <Time nome="Programação"/>
-      <Time nome="Front-end"/>
-      <Time nome="Data Science"/>
+      <Banner />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdd(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Front-end" />
+      <Time nome="Data Science" />
     </div>
   );
 }
