f0f9317c69739ac4ef9e6bdeb399a6671fff9dbe
Alexandre <alexandrefaria3012@gmail.com>
2024-08-08 17:12:30 -0300

    Update time.js

--- src/componentes/Time/time.js
+++ src/componentes/Time/time.js
@@ -1,9 +1,14 @@
 import './time.css'
 
 const Time = (props) => {
+
+    const css = {backgroundColor:props.corSecundaria}
+    const css2 = {borderColor:props.corPrimaria}
+
     return (
-        <section className='time'>
-            <h3 className='time-titulo'>{props.nome}</h3>
+
+        <section className='time' style={css}>
+            <h3 className='time-titulo' style={css2}>{props.nome}</h3>
         </section>
     )
 }
