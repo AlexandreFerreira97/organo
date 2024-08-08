8f8b7162058a324aac70d90894225e53df1d104a
Alexandre <alexandrefaria3012@gmail.com>
2024-08-08 17:48:14 -0300

    Update colaborador.js

--- src/componentes/Colaborador/colaborador.js
+++ src/componentes/Colaborador/colaborador.js
@@ -2,7 +2,17 @@ import './colaborador.css'
 
 const Colaborador = () => {
     return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img className='img' src='https://github.com/AlexandreFerreira97.png' alt='Alexandre Ferreira'/>
+            </div>
 
+            <div className='rodape'>
+                <h4>Alexandre Ferreira</h4>
+                <h5>Desenvolvedor front-end</h5>
+            </div>
+
+        </div>
     )
 }
 
